5945eceb176aee4d34ea9b8ab888202a9cdb5549
Syahrul Setiawan <syahrulsetiawan2801@gmail.com>
2025-06-22 11:22:03 +0700

    Migrasi ke Server side

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home | Web Informatika</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Exile&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css" >
+  </head>
+
+  <body>
+    <h1>INFORMATIKA KELAS F</h1>
+    <hr width="50%" align="center" size="2px" color="black"></hr>
+
+    <nav>
+        <ul style="list-style-type: none; text-align: center; padding: 0;">
+            <li style="display: inline; margin:0 15px; text-decoration: none;">
+                <a href="index.html">Home</a>
+            </li>
+            <li style="display: inline; margin:0 15px; text-decoration: none;">
+                <a href="#about">About</a>
+            </li>
+            <li style="display: inline; margin:0 15px; text-decoration: none;">
+                <a href="service.html">Service</a>
+            </li>
+            <li style="display: inline; margin:0 15px; text-decoration: none;">
+                <a href="#contact">Contact Us</a>
+            </li>
+            <li style="display: inline; margin:0 15px; text-decoration: none;">
+                <a href="login.html">Login</a>
+            </li>
+        </ul>
+    </nav>
+
+    <hr width="50%" align="center" size="2px" color="black"></hr>
+
+    <h1 align="center">
+        <img src="https://unimus.ac.id/wp-content/uploads/2023/09/logo-unggul-small.png" height="200px">
+    </h1>
+
+    <p>
+        <b><i>Lorem Ipsum</i></b> dolor sit amet, consectetur adipisicing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+
+    <h2>DAFTAR MATA KULIAH SEMESTER 4:</h2>
+    <ol>
+        <li>Pemrogaram Berbasis Web</li>
+        <ul>
+            <li>Dosen : Basirudin Ansor, M.Kom</li>
+        </ul>
+        <li>Pengolahan Citra Digital</li>
+        <ul>
+            <li>Dosen : Dr. Muhammad Munsarif, S.Kom, M.Kom</li>
+        </ul>
+        <li>Internet Of Things</li>
+        <ul>
+            <li>Dosen : Arief Hendra Saptadi, S.T., M.Eng</li>
+        </ul>
+        <li>Interaksi Manusia dan Komputer</li>
+        <ul>
+            <li>Dosen : Asdani Kindarto, ST, M.Eng, Phd</li>
+        </ul>
+    </ol>
+
+  </body>
+</html>
